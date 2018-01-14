7c12b5a9761e46484f6b73d9d6e56389d01cecc8
MYTH-INF <maytheinfante96@gmail.com>
2018-01-13 20:30:05 -0500

    modificando vista perfil

--- js/perfil.js
+++ js/perfil.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  var $textarea = $('#insert-text');
+  var $textarea = $('.materialize-textarea');
   var $btnPosting = $('.btn-posting');
   var $divContainerPosts = $('#div-posts');
   var $btnFollow = $('.btn-follow');
@@ -15,7 +15,7 @@ $(document).ready(function() {
   //  agregando evento al boton para guardar las publicaciones en un contenedor
   $btnPosting.on('click', function() {
     //  agregando hora de publicación
-    $divContainerPosts .append('<div class="container-posts">' + $textarea.val() + '</div>');
+    $divContainerPosts .append('<div class="container-posts col s12">' + $textarea.val() + '</div>');
     $divContainerPosts .append('<div class="time">' + moment().format('LLLL') + '</div>');
   });
   //  agregando funcion Seguir (al botón seguir)
@@ -26,12 +26,5 @@ $(document).ready(function() {
   });
   //  agregando nickName de registro
   var $nickName = localStorage.getItem('nick-name');
-  $('h6>strong').append($nickName);
-  //  CREANDO VARIALE PARA IMAGENES DE FRASES ALEATORIAS
-  var min = 1;
-  var max = 5;
-  var aleatorio = Math.floor(Math.random() * (max - (min - 1))) + min;
-  var imagePhrase = '<img src="../assets/images/frase' + aleatorio + '.png' + '">';
-  var $frase = $('.frase');
-  $frase.append(imagePhrase);
+  $('h4').append($nickName);
 });
